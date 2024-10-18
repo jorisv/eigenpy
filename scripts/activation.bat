649fa6ba029c7f34b3710e3df610c080a6d73850
Joris Vaillant <joris.vaillant@inria.fr>
2024-10-18 12:15:59 +0200

    pixi: Add some comments

--- scripts/activation.bat
+++ scripts/activation.bat
@@ -7,6 +7,7 @@ set CMAKE_EXPORT_COMPILE_COMMANDS=1
 :: Activate color output with Ninja
 set CMAKE_COLOR_DIAGNOSTICS=1
 
+:: Set default build value only if not previously set
 if not defined EIGENPY_BUILD_TYPE (set EIGENPY_BUILD_TYPE=Release)
 if not defined EIGENPY_PYTHON_STUBS (set EIGENPY_PYTHON_STUBS=ON)
 if not defined EIGENPY_CHOLMOD_SUPPORT (set EIGENPY_CHOLMOD_SUPPORT=OFF)
