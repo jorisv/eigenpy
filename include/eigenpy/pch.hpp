8b0bac5a6804af8653e1baab4e7a2431394d9990
Joris Vaillant <joris.vaillant@inria.fr>
2024-11-13 15:38:47 +0100

    cmake: Try to use pre compiled header to accelerate the build

---
+++ include/eigenpy/pch.hpp
@@ -0,0 +1,8 @@
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
+#include <boost/python.hpp>
+#include <boost/type_traits.hpp>
+
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include <Eigen/Geometry>
+#include <unsupported/Eigen/CXX11/Tensor>
